b82612278f59497a808a1c324207d8171c9acbe8
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-08-24 21:16:34 +0600

    Lesson 3 SOLUTION.sql file was added SET and changed to appropriate syntax

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,18 +1,13 @@
-ALTER TABLE Student
-ALTER COLUMN birthday date NOT NULL;
+ALTER TABLE Student ALTER COLUMN birthday SET NOT NULL;
 
-ALTER TABLE Mark
-ALTER COLUMN mark WHERE mark 1 AND 10,
-ALTER COLUMN student_id bigint NOT NULL,
-ALTER COLUMN subject_id bigint NOT NULL;
+ALTER TABLE Mark ADD CONSTRAINT mark_check CHECK (mark >= 1 AND mark <= 10);
+ALTER TABLE Mark ALTER COLUMN student_id SET NOT NULL;
+ALTER TABLE Mark ALTER COLUMN subject_id SET NOT NULL;
 
-ALTER TABLE Subject
-ALTER COLUMN grade WHERE grade 1 AND 5;
+ALTER TABLE Subject ADD CONSTRAINT grade_check CHECK (grade >= 1 AND grade <= 5);
 
-ALTER TABLE PaymentType
-ADD CONSTRAINT unique_payment_type_name UNIQUE (name);
+ALTER TABLE PaymentType ADD CONSTRAINT unique_payment_type_name UNIQUE (name);
 
-ALTER TABLE Payment
-ALTER COLUMN type_id bigint NOT NULL,
-ALTER COLUMN amount decimal NOT NULL,
-ALTER COLUMN payment_date DateTime NOT NULL;
+ALTER TABLE Payment ALTER COLUMN type_id SET NOT NULL;
+ALTER TABLE Payment ALTER COLUMN amount SET NOT NULL;
+ALTER TABLE Payment ALTER COLUMN payment_date SET NOT NULL;
