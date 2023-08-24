3a74ce36497c0089fcfd87e2511f51402c6f9d20
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-08-24 19:25:59 +0600

    Lesson 10 SOLUTION.sql file was changed from function SUM to AVG

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
 SELECT s.* FROM Subject s JOIN Mark m ON s.id = m.subject_id GROUP BY s.id HAVING AVG(m.mark) > (SELECT AVG(mark) FROM Mark );
 
-SELECT s.* FROM Student s JOIN Payment p ON s.id = p.student_id GROUP BY s.id HAVING SUM(p.amount) > (SELECT AVG(amount) FROM Payment);
+SELECT s.* FROM Student s JOIN Payment p ON s.id = p.student_id GROUP BY s.id HAVING AVG(p.amount) < (SELECT AVG(amount) FROM Payment);
