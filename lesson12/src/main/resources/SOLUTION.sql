fa00ec4018b3384707f2134d78ffd477ac5809a3
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-08-25 18:18:19 +0600

    Lesson 12 SOLUTION.sql, was changed

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,7 +1,12 @@
-DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark m JOIN Subject s ON s.id = m.subject_id WHERE s.grade >= 4;
+--DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark m JOIN Subject s ON s.id = m.subject_id WHERE s.grade >= 4);
+DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark >= 4);
+DELETE FROM Payment WHERE student_id IN (SELECT student_id FROM Mark WHERE mark >= 4);
+DELETE FROM Mark WHERE mark >= 4;
 
+DELETE FROM Mark WHERE student_id IN (SELECT DISTINCT student_id FROM Mark WHERE mark < 4);
 DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark WHERE mark < 4);
 
+DELETE FROM Payment WHERE type_id IN (SELECT id FROM PaymentType WHERE name = 'Daily');
 DELETE FROM PaymentType WHERE name = 'Daily';
 
 DELETE FROM Mark WHERE mark < 7;
