f09c8c18f03affe7477efe391c51bf1d5e877dee
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-08-30 00:02:12 +0600

    Lesson 12 SOLUTION.sql and TABLES_CHANGES.sql were changed

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark m JOIN Subject s ON s.id = m.subject_id WHERE grade >= 4);
+DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark m JOIN Subject s ON s.id = m.subject_id WHERE m.mark >= 4);
 
 DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark WHERE mark < 4);
 
