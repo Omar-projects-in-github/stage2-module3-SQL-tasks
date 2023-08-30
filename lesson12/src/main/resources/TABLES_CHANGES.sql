5498b9cf9af90ff9715df4c57131059d0816b241
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-08-30 22:24:23 +0600

    Lesson 12. TABLES_CHANGES.sql was changed a little bit

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,6 +1,8 @@
 ALTER TABLE Mark DROP CONSTRAINT mark_student_id_fkey;
 ALTER TABLE Mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
-ALTER TABLE Payment DROP CONSTRAINT payment_type_id_fkey;
-ALTER TABLE Payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES PaymentType(id) ON DELETE CASCADE;
+ALTER TABLE Mark DROP CONSTRAINT mark_subject_id_fkey;
+ALTER TABLE Mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) REFERENCES Subject(id) ON DELETE CASCADE;
 ALTER TABLE Payment DROP CONSTRAINT payment_student_id_fkey;
 ALTER TABLE Payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
+ALTER TABLE Payment DROP CONSTRAINT payment_type_id_fkey;
+ALTER TABLE Payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES PaymentType(id) ON DELETE CASCADE;
